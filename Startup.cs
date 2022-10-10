a2be670dc5ffc8fb88719c708272a2187ba54019
olafire <olafire03@gmail.com>
2022-10-10 10:13:09 +0100

    deploy

--- Startup.cs
+++ Startup.cs
@@ -84,7 +84,7 @@ namespace MCake_Manage
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            //SetupDb.SeedData(userManager, roleManager);
+            SetupDb.SeedData(userManager, roleManager);
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseRouting();
