829a0c85652dbfc22ef166997bd38da0e0b70235
olafire <olafire03@gmail.com>
2022-10-12 01:38:14 +0100

    chc

--- Startup.cs
+++ Startup.cs
@@ -95,8 +95,8 @@ namespace MCake_Manage
             app.UseAuthorization();
             app.UseStaticFiles(new StaticFileOptions()
             {
-                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot", "StaticFiles")),
-                RequestPath = new PathString("/wwwroot/StaticFiles")
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"StaticFiles")),
+                RequestPath = new PathString("/StaticFiles")
             });
             app.UseEndpoints(endpoints =>
             {
